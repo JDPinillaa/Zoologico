aef6819ced7d50266bda363874f924f7206997cb
Juan Diego Pinilla <juand.pinillaa@autonoma.edu.co>
2026-09-16 09:15:41 -0500

    agrega relacion entre Zona y Especie

--- src/main/java/entidades/Zona.java
+++ src/main/java/entidades/Zona.java
@@ -1,8 +1,13 @@
 package entidades;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Zona {
     private String nombre;
     private double extencionMetrosCuadrados;
+
+    private List<Especie> especies = new ArrayList<>();
 
     public Zona(String nombre, double extencionMetrosCuadrados) {
         this.nombre = nombre;
@@ -23,5 +28,17 @@ public class Zona {
 
     public void setExtencionMetrosCuadrados(double extencionMetrosCuadrados) {
         this.extencionMetrosCuadrados = extencionMetrosCuadrados;
+    }
+
+    public List<Especie> getEspecies() {
+        return especies;
+    }
+
+    public void setEspecies(List<Especie> especies) {
+        this.especies = especies;
+    }
+
+    public void agregarEspecie(Especie especie){
+        especies.add(especie);
     }
 }
