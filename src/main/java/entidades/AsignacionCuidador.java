08684ec5cb004dd9fc58066396fb3376f17a3694
Juan Diego Pinilla <juand.pinillaa@autonoma.edu.co>
2026-09-16 09:25:29 -0500

    Añade relacion entre Cuidador y Especie

--- src/main/java/entidades/AsignacionCuidador.java
+++ src/main/java/entidades/AsignacionCuidador.java
@@ -3,5 +3,37 @@ package entidades;
 import java.util.Date;
 
 public class AsignacionCuidador {
+    private Cuidador cuidador;
+    private Especie especie;
     private Date fecha;
+
+    public AsignacionCuidador(Cuidador cuidador, Especie especie, Date fecha) {
+        this.cuidador = cuidador;
+        this.especie = especie;
+        this.fecha = fecha;
+    }
+
+    public Cuidador getCuidador() {
+        return cuidador;
+    }
+
+    public void setCuidador(Cuidador cuidador) {
+        this.cuidador = cuidador;
+    }
+
+    public Especie getEspecie() {
+        return especie;
+    }
+
+    public void setEspecie(Especie especie) {
+        this.especie = especie;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
 }
