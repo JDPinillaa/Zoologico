3ef3d538390e8fe14f31a28db2635116296d9a08
Juan Diego Pinilla <juand.pinillaa@autonoma.edu.co>
2026-09-16 09:30:27 -0500

    Agrega relacion entre Guia y Itinerario

--- src/main/java/entidades/AsignacionGuia.java
+++ src/main/java/entidades/AsignacionGuia.java
@@ -4,4 +4,12 @@ import java.sql.Time;
 
 public class AsignacionGuia {
     private Time hora;
+    private Guia guia;
+    private Itinerario itinerario;
+
+    public AsignacionGuia(Time hora, Guia guia, Itinerario itinerario) {
+        this.hora = hora;
+        this.guia = guia;
+        this.itinerario = itinerario;
+    }
 }
